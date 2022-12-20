8d3211aba0143702c9af1946860475313b98e98c
Roman <rkozhevnikov00@gmail.com>
2022-12-21 00:18:16 +0300

    Причесал Transfers

--- src/main/java/com/alfa/SettingsOption.java
+++ src/main/java/com/alfa/SettingsOption.java
@@ -1,53 +1,68 @@
 package com.alfa;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
 
+import static java.util.Collections.singletonMap;
+
 
 public class SettingsOption {
 
 
-    public static HashMap<String, Boolean> getExcludedSchemes() {
-        HashMap<String, Boolean> allowedProtocolKeys = new HashMap<>();
-        allowedProtocolKeys.put("alfadirect-sign-insecure", false);
-        allowedProtocolKeys.put("alfadirect-sign", false);
-        allowedProtocolKeys.put("adir-test-insecure", false);
-        allowedProtocolKeys.put("adir-test", false);
-        return allowedProtocolKeys;
-    }
+//    public static HashMap<String, Boolean> getExcludedSchemes() {
+//        HashMap<String, Boolean> allowedProtocolKeys = new HashMap<>();
+//        allowedProtocolKeys.put("alfadirect-sign-insecure", false);
+//        allowedProtocolKeys.put("alfadirect-sign", false);
+//        allowedProtocolKeys.put("adir-test-insecure", false);
+//        allowedProtocolKeys.put("adir-test", false);
+//        return allowedProtocolKeys;
+//    }
+//
+//    public static HashMap<String, Boolean> getAllowedProtocolKeys() {
+//        HashMap<String, Boolean> allowedProtocolKeys = new HashMap<>();
+//        allowedProtocolKeys.put("alfadirect-sign-insecure", true);
+//        allowedProtocolKeys.put("alfadirect-sign", true);
+//        allowedProtocolKeys.put("adir-test-insecure", true);
+//        allowedProtocolKeys.put("adir-test", true);
+//        return allowedProtocolKeys;
+//    }
+//
+//    public static HashMap<String, Object> getAllowedOriginProtocols() {
+//        HashMap<String, Object> allowedProtocols = new HashMap<>();
+//        allowedProtocols.put("http://localhost:56491", getAllowedProtocolKeys());
+//        allowedProtocols.put("https://ip12.alfadirect.ru", getAllowedProtocolKeys());
+//        allowedProtocols.put("https://ip16.alfadirect.ru", getAllowedProtocolKeys());
+//        allowedProtocols.put("https://lkpreprod.alfadirect.ru", getAllowedProtocolKeys());
+//        return allowedProtocols;
+//    }
 
-    public static HashMap<String, Boolean> getAllowedProtocolKeys() {
-        HashMap<String, Boolean> allowedProtocolKeys = new HashMap<>();
-        allowedProtocolKeys.put("alfadirect-sign-insecure", true);
-        allowedProtocolKeys.put("alfadirect-sign", true);
-        allowedProtocolKeys.put("adir-test-insecure", true);
-        allowedProtocolKeys.put("adir-test", true);
-        return allowedProtocolKeys;
-    }
 
-    public static HashMap<String, Object> getAllowedOriginProtocols(){
-        HashMap<String, Object> allowedProtocols = new HashMap<>();
-        allowedProtocols.put("http://localhost:56491", getAllowedProtocolKeys());
-        allowedProtocols.put("https://ip12.alfadirect.ru", getAllowedProtocolKeys());
-        allowedProtocols.put("https://ip16.alfadirect.ru", getAllowedProtocolKeys());
-        allowedProtocols.put("https://lkpreprod.alfadirect.ru", getAllowedProtocolKeys());
-        return allowedProtocols;
-    }
 
     public static ChromeDriver getChromeDriver() {
         ChromeOptions chromeOptions = new ChromeOptions();
-//        chromeOptions.setCapability("protocol_handler.excluded_schemes", getExcludedSchemes());
-//        chromeOptions.setCapability("protocol_handler.allowed_origin_protocol_pairs", getAllowedOriginProtocols());
-        chromeOptions.addArguments("window-size=1920,1080");
-//        chromeOptions.setCapability("safebrowsing", "enabled");
+
+        WebDriverManager.chromedriver().setup();
+        HashMap<String, Object> prefs = new HashMap<>();
+        prefs.put(
+                "protocol_handler.allowed_origin_protocol_pairs",
+                singletonMap("https://ip12.alfadirect.ru", singletonMap("adir-test", true))
+        );
+        chromeOptions.setExperimentalOption("prefs", singletonMap("protocol_handler.allowed_origin_protocol_pairs", singletonMap("https://ip12.alfadirect.ru", singletonMap("adir-test", true))));
+
+        chromeOptions.addArguments("window-size=1280,720");
+        chromeOptions.addArguments("--headless");
+
+
         chromeOptions.addArguments("--safebrowsing-disable-download-protection");
         chromeOptions.addArguments("--safebrowsing-disable-extension-blacklist");
         chromeOptions.addArguments("--disable-extensions");
         chromeOptions.addArguments("--disable-dev-shm-usage");
         chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("--headless");
+
+
 //        chromeOptions.addArguments("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36");
 //        chromeOptions.addArguments("user-agent=Windows");
 //        chromeOptions.addArguments("--ignore-certificate-errors");
@@ -71,8 +86,8 @@ public class SettingsOption {
 //        chromeOptions.addArguments("--disable-infobars");
 //        chromeOptions.addArguments("--enable-infobars");
 //        chromeOptions.addArguments("--kiosk-printing");
-        chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addArguments("--disable-infobars");
+//        chromeOptions.addArguments("--disable-notifications");
+//        chromeOptions.addArguments("--disable-infobars");
         return new ChromeDriver(chromeOptions);
     }
 
--- src/test/java/com/alfa/TransfersTest.java
+++ src/test/java/com/alfa/TransfersTest.java
@@ -4,15 +4,10 @@ package com.alfa;
 import com.alfa.authorize.LoginPage;
 import com.alfa.moneytransfers.InteriorPage;
 import com.alfa.moneytransfers.OtherBankPage;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.sikuli.script.FindFailed;
 
-import java.net.URISyntaxException;
 import java.time.Duration;
 
 
@@ -20,25 +15,21 @@ public class TransfersTest {
 
     WebDriver driver;
     WebDriverWait webDriverWait;
-    static ChromeOptions chromeOptions = new ChromeOptions();
+
 
     private final static String LK_TEST_URL = "https://ip12.alfadirect.ru";
 
 
     @BeforeAll
     static void registerDriver() {
-        WebDriverManager.chromedriver().setup();
-        chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("window-size=1920,1080");
-
 
 
     }
 
     @BeforeEach
     void setupBrowser() throws InterruptedException {
-        driver = new ChromeDriver();
-//        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver = SettingsOption.getChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
         driver.get(LK_TEST_URL);
 
 
@@ -55,15 +46,15 @@ public class TransfersTest {
 
     @Test
     @DisplayName("Проверка перевода в другой банк ")
-    void otherBankTransfer() throws FindFailed, URISyntaxException {
+    void otherBankTransfer()  {
 
 
         new OtherBankPage(driver)
                 .chooseInTheOtherBank()
                 .insertFields()
                 .clickSendMoney()
-                .pressKeys()
-                .checkSuccessNotify();;
+                .checkSuccessNotify();
+
     }
 
     @Test
